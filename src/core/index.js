cdff849207a1d3ea6cacdea9d3aa61579bdc7dfe
Rayler Ramirez | BiteFixes <121004818+raylerr481@users.noreply.github.com>
2026-08-23 15:25:31 -0300

    Expose Bitey IA supracerebro foundation modules

--- src/core/index.js
+++ src/core/index.js
@@ -2,3 +2,5 @@ export { BiteySupracerebro } from './suprabrain.js';
 export { ContextEngine } from './context.js';
 export { MemoryEngine } from './memory.js';
 export { ResearchEngine } from './research.js';
+export { EnterpriseContext } from './enterprise-context.js';
+export { ProviderRegistry } from './provider-registry.js';
